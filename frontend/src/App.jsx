46581c177c1a5c57f790ebf6d28b5222e0b2d29f
Satyajit Nayak <satyajitnayak348@gmail.com>
2024-04-10 07:39:28 +0530

    pages routes setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,31 @@
 import { useState } from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import './App.css'
+import LoginPage from './pages/authPages/LoginPage/LoginPage'
+import RegisterPage from './pages/authPages/RegisterPage/RegisterPage'
+import Dashboard from './pages/Dashboard/Dashboard'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
-      Hello, here is a React app
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/login'
+            element={<LoginPage />}
+          />
+          <Route path='/register'
+            element={<RegisterPage />}
+          />
+          <Route path='/dashboard'
+            element={<Dashboard />}
+          />
+          <Route path='/' element={<Navigate to='/dashboard' />} />
+          <Route path='*' element={<h1>Page not Found 😭</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
