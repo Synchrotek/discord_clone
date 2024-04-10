8df22f1181f6bc4452889f6156cdd8d4a83c4f87
Satyajit Nayak <satyajitnayak348@gmail.com>
2024-04-10 07:22:07 +0530

    css remove & readmeU

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
 import store from './store/store.js'
 import { Provider } from 'react-redux'
--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
 ### A collaboration platform 
-- Like a lite version of dicord
+- Like a lite version of discord
 - with some of it's core features
