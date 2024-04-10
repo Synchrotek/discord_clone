5b6b00aa6668670c5447e4a30da7c67c74f9f0bd
Satyajit Nayak <satyajitnayak348@gmail.com>
2024-04-10 06:53:52 +0530

    react, redux, tailwind setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div
+      className='bg-slate-300 text-blue-800 text-xl p-3 rounded-xl font-semibold'
+    >DISCORD_CLONE
+    </div>
+  )
+}
+
+export default App
+
+
